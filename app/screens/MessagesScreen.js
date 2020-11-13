82da63e75d76ce2789550eadb7bdf4455e76ffd3
Sicabou <pbouriat@audencia.com>
2020-11-13 12:09:36 +0100

    GItHub upload

---
+++ app/screens/MessagesScreen.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { FlatList, StyleSheet, View } from "react-native";
+
+import Screen from "../components/Screen";
+import ListItem from "../components/ListItem";
+import ListItemSeparator from "../components/ListItemSeparator";
+import ListItemDeleteAction from "../components/ListItemDeleteAction";
+
+const messages = [
+  {
+    id: 1,
+    title: "T1",
+    description: "D1",
+    image: require("../assets/mosh.jpg"),
+  },
+  {
+    id: 2,
+    title: "T2",
+    description: "D2",
+    image: require("../assets/mosh.jpg"),
+  },
+  {
+    id: 3,
+    title: "T3",
+    description: "D3",
+    image: require("../assets/mosh.jpg"),
+  },
+];
+
+function MessagesScreen(props) {
+  return (
+    <Screen>
+      <FlatList
+        data={messages}
+        keyExtractor={(message) => message.id.toString()}
+        renderItem={({ item }) => (
+          <ListItem
+            title={item.title}
+            subtitle={item.description}
+            image={item.image}
+            onPress={() => console.log("Message selected", item)}
+            renderRightActions={ListItemDeleteAction}
+          />
+        )}
+        ItemSeparatorComponent={ListItemSeparator}
+      />
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default MessagesScreen;
