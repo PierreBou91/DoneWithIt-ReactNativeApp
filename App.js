a284d2c4ccd378558be10f4379b026dba195451f
Sicabou <pbouriat@audencia.com>
2020-11-13 15:02:44 +0100

    AppTextInput

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import MessagesScreen from "./app/screens/MessagesScreen";
 import MyAccountScreen from "./app/screens/AccountScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
@@ -7,7 +7,16 @@ import Icon from "./app/components/Icon";
 import ListItem from "./app/components/ListItem";
 import AccountScreen from "./app/screens/AccountScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
+import { Text, TextInput } from "react-native";
+import AppTextInput from "./app/components/AppTextInput";
+import colors from "./app/config/colors";
 
 export default function App() {
-  return <ListingsScreen />;
+  const [firstName, setFirstName] = useState("");
+
+  return (
+    <Screen>
+      <AppTextInput placeholder="Username" icon="email" />
+    </Screen>
+  );
 }
